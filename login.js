9b2530eb1e51ca9a39585867cb0ddbd26ed299f4
Chang ZHAO <czha9316@uni.sydney.edu.au>
2021-05-29 17:21:33 +1000

    v2

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-/* This function changes the header by showing the person has 
+/* This function changes the header by showing the person has
  * successfully login to the system
  */
 function updateHeader(name,pwd)
@@ -6,27 +6,30 @@ function updateHeader(name,pwd)
     // check password
     secret = pwd.value;
     user = name.value;
-    
+
     if(pwdOK(user, secret))
     {
 	    pwd.value="";
-        window.location.replace("account.html");
-        return;
-    	
+      //  window.location.replace("account.html");
+
+
         // prepare new H1 tag
 	    h1Text = document.createTextNode(user + " has successfully logged in");
 
         // remove existing H1
 	    target = document.getElementById("h1ID1");
-        target.removeChild(target.firstChild);
-	    target.style.color = "red";
+      target.removeChild(target.firstChild);
+	    target.style.color = "white";
 	    target.style.fontStyle = "italic";
- 
+
+      target2 = document.getElementById("loginForm");
+      target2.style.visibility = "hidden";
+
         // add new H1 tag to the webpage & reset input field
         target.appendChild(h1Text);
         name.value ="";
     }
-    
+
     pwd.value="";
 }
 
